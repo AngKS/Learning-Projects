297ce76ca23b0c23ce19f284e3280d1fc3bbbe2c
AKS <60446729+AngKS@users.noreply.github.com>
2023-08-06 09:38:25 +0000

    WIP for learning vanilla drag-and-drop

---
+++ Vanilla_Drag_and_Drop/script.js
@@ -0,0 +1,13 @@
+
+
+// get body tag
+const body = document.querySelector('body');
+
+// add a box element to the body
+const elem = `<div class="box"></div>`;
+
+// add a box element to the body
+body.insertAdjacentHTML('beforeend', elem);
+
+ 
+
